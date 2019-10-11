faf65288cd71f9d3b579dd69d4c24fe64f8a09b1
Miroslav Krsjak <miroslav.krsjak@sk.ibm.com>
2019-10-11 22:25:13 +0200

    1

--- octoprint_enclosure/getSensors.py
+++ octoprint_enclosure/getSensors.py
@@ -15,7 +15,7 @@ def readValue(sensor_name):
     lines = output.split('\n')
     lines = filter(None, lines)
     value = -1
-    if len(lines) <> 1:
+    if len(lines) != 1:
         error()
     if lines[0].find(sensor_name) >= 0:
         values = lines[0].split()
@@ -23,19 +23,19 @@ def readValue(sensor_name):
         value = float(numeric)
     return value
 
-#temp_name = "???"
-#humidity_name = "???"
 
-# if len(sys.argv) == 3:
-#    temp_name = sys.argv[1]
-#    humidity_name = sys.argv[2]
-# else:
-#    error()
+temp_name = "???"
+humidity_name = "???"
 
+if len(sys.argv) == 3:
+    temp_name = sys.argv[1]
+    humidity_name = sys.argv[2]
+else:
+    error()
 
-#cTemp = readValue(temp_name)
-#humidity = readValue(humidity_name)
-cTemp = -1
-humidity = -1
+cTemp = readValue(temp_name)
+humidity = readValue(humidity_name)
+#cTemp = -1
+#humidity = -1
 
 print('{0:0.1f} | {1:0.1f}'.format(cTemp, humidity))
