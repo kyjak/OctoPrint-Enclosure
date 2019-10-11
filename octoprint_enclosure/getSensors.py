8b3b7b81b44e0df0e3995ee5ea8aaae8269a16ef
Miroslav Krsjak <miroslav.krsjak@sk.ibm.com>
2019-10-11 22:09:10 +0200

    2

--- octoprint_enclosure/getSensors.py
+++ octoprint_enclosure/getSensors.py
@@ -1,17 +1,17 @@
-import smbus
-import time
 import sys
 import subprocess
+
 
 def error():
     print('-1 | -1')
     sys.exit(1)
 
-def readValue(sensor_name):    
+
+def readValue(sensor_name):
     cmd = "sensors | grep -e " + sensor_name
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
     (output, err) = p.communicate()
-    p_status = p.wait()    
+    p_status = p.wait()
     lines = output.split('\n')
     lines = filter(None, lines)
     value = -1
@@ -23,16 +23,19 @@ def readValue(sensor_name):
         value = float(numeric)
     return value
 
-temp_name = "???"
-humidity_name = "???"
+#temp_name = "???"
+#humidity_name = "???"
 
-if len(sys.argv) == 3:
-    temp_name = sys.argv[1]
-    humidity_name = sys.argv[2]
-else:
-    error()
+# if len(sys.argv) == 3:
+#    temp_name = sys.argv[1]
+#    humidity_name = sys.argv[2]
+# else:
+#    error()
 
-cTemp = readValue(temp_name)
-humidity = readValue(humidity_name)
-            
+
+#cTemp = readValue(temp_name)
+#humidity = readValue(humidity_name)
+cTemp = -1
+humidity = -1
+
 print('{0:0.1f} | {1:0.1f}'.format(cTemp, humidity))
